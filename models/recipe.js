e2952f57f8ef8c82c3085eb79f2aa16ddb286b26
pierre-canet <pierre.canet@laplateforme.io>
2026-01-07 14:55:27 +0100

    v0.7 mise en place du système de routing

--- models/recipe.js
+++ models/recipe.js
@@ -2,13 +2,9 @@ const mongoose = require("mongoose");
 
 const recipeSchema = mongoose.Schema({
   title: { type: String, required: true },
-
   ingredients: { type: String, required: true },
-
   steps: { type: String, required: true },
-
   userId: { type: String, required: true },
-
   date: { type: String, required: true },
 });
 
